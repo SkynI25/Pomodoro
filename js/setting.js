f226891e620ef347d1cd093b151a1d3391b67fa1
SkynI25 <rlxo1651@daum.net>
2020-05-01 17:11:00 +0900

    modify setting logic by using regex & coloring input box when user entering wrong value

--- js/setting.js
+++ js/setting.js
@@ -1,54 +1,66 @@
 ﻿const val1N = "Pomo Time";
 const val2N = "Break Time";
 const val3N = "Big Break Time";
+const wrongInput = `wrong-input`;
+const pomoTime = document.querySelector('.pomoTime');
+const breakTime = document.querySelector('.breakTime');
+const bigbreakTime = document.querySelector('.bigbreakTime');
+const timesArr = [
+    {type: pomoTime, range: 120},
+    {type: breakTime, range: 30},
+    {type: bigbreakTime, range: 60},
+];
+
+function pointWrongInput(data) {
+    const value = data.type.value;
+    const range = data.range;
+    const type = data.type;
+    if(!valiCheck(value, range)) {
+        type.classList.add(wrongInput);
+    } else if(valiCheck(value, range) && type.classList.contains(wrongInput)) {
+        type.classList.remove(wrongInput);
+    }
+}
 
 function setChildValue() {
-    const val1 = document.querySelectorAll('.pomoTime')[0].value;
-    const val2 = document.querySelectorAll('.breakTime')[0].value;
-    const val3 = document.querySelectorAll('.bigbreakTime')[0].value;
-
     let listVal = [];
-    let listValN = [];
+    const val1 = timesArr[0].type.value;
+    const val2 = timesArr[1].type.value;
+    const val3 = timesArr[2].type.value;
 
     listVal.push(val1);
     listVal.push(val2);
     listVal.push(val3);
 
-    listValN.push(val1N);
-    listValN.push(val2N);
-    listValN.push(val3N);
-
-    if(valiCheck(val1) && valiCheck(val2) && valiCheck(val3)) {
-        let r = confirm("Do you want to set the time?");
-        if(r == true) {
+    if(valiCheck(val1, timesArr[0].range) && valiCheck(val2, timesArr[1].range) && valiCheck(val3, timesArr[2].range)) {
+        if(confirm("Save the times?")) {
             sessionStorage.setItem("timeSetting", listVal);
             window.location.href='main.html';
         }
     } else {
-        alert(validationDetector(listValN, listVal))
+        pointWrongInput(timesArr[0]);
+        pointWrongInput(timesArr[1]);
+        pointWrongInput(timesArr[2]);
+        alert(`Please setting the time with a valid number.`);
     }
 }
 
-function valiCheck(data) {
-    if(data !== undefined && data !== '' && data !== null && Number.isInteger(parseInt(data))) {
-        return true;
+function valiCheck(data,range) {
+    return !/\D/.test(data) && Number(data) > 0 && Number(data) <= range; 
+}
+
+function initInput() {
+    if(sessionStorage.getItem('timeSetting')) {
+        const values = sessionStorage.getItem('timeSetting').split(",");
+        pomoTime.value = values[0];
+        breakTime.value = values[1];
+        bigbreakTime.value = values[2];
     }
-    return false;
 }
 
-function validationDetector(inputId, data2) {
-    let result = '';
-    data2.some((el,idx) => {
-        if(parseInt(el) === '' || !Number.isInteger(parseInt(el))) {
-            result = `Please input on ${inputId[idx]} as a number`;
-            return true;
-        }
-        if(parseInt(el) < 0 && parseInt(el) > 1440) {
-            result = `Please input on ${inputId[idx]} between the range as a number`;
-            return true;
-        }
-    });
-    return result;
+function init() {
+    initInput();
+    document.querySelector('#setTimeValue').addEventListener('click', setChildValue);
 }
 
-document.querySelector('#setTimeValue').addEventListener('click', setChildValue);
+init();
