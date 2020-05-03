5aec7e8b4f48f932d41a060857f2056ef2ce023d
SkynI25 <rlxo1651@daum.net>
2020-05-03 19:39:24 +0900

    modify stop watch feature(setInterval to setTimeout)

--- js/pomo.js
+++ js/pomo.js
@@ -1,134 +1,107 @@
-﻿function pad(n, width, z) {
+﻿let minutes = 0;
+let timerHour = 0;
+let currSetNum = 1;
+let timerMinute = 0;
+let nTimes4Compute = 0;
+const tids = [];
+const startBtn = document.querySelector('#startbtn');
+const numOfSet = document.querySelector('#numOfset');
+const setPomoNum = document.querySelector('#setPomoNum');
+const sessionValue = sessionStorage.getItem('timeSetting') === null ? [] : sessionStorage.getItem('timeSetting').split(',');
+
+function pad(n, width, z) {
     z = z || '0';
     n = n + '';
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-let value = sessionStorage.getItem('timeSetting') == null ? [] : sessionStorage.getItem('timeSetting').split(',');
-
-let timer = sessionStorage.getItem('timeSetting') == null ? 0 : null,
-    setNum = isNaN(parseInt(sessionStorage.getItem('setNum'))) ? 0 : parseInt(sessionStorage.getItem('setNum')),
-    pomoNum = isNaN(parseInt(sessionStorage.getItem('pomoNum'))) ? 0 : parseInt(sessionStorage.getItem('pomoNum'));
-
-let timer2 = null,
-    countDownDate2;
-
-let timer3 = null,
-    countDownDate3;
-
-let isBeginning = 0;
-let distance = 0;
-
-let countDownDate4Display = new Date().getTime() + parseFloat(value[0]) * 60000;
 let now4Display = new Date().getTime();
+let countDownDate4Display = new Date().getTime() + parseFloat(sessionValue[0]) * 60000;
 let distance4Display = countDownDate4Display - now4Display;
 let hours4Display = Math.floor((distance4Display % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes4Display = Math.floor((distance4Display % (1000 * 60 * 60)) / (1000 * 60));
 let seconds4Display = Math.floor((distance4Display % (1000 * 60)) / 1000);
 
-window.onload = _ => {
-    if(timer !== null) {
-        alert('Please set the times first in the setting')
-        return;
-    }
-    document.getElementById('setPomoNum').innerHTML = pad(hours4Display, 2) + ":" + pad(minutes4Display, 2) + ":" + pad(seconds4Display, 2);
-    if(!isNaN(parseInt(sessionStorage.getItem('pomoNum')))) {
-        document.getElementById('numOfset').innerHTML = parseInt(sessionStorage.getItem('pomoNum')) + ` POMO Concentration`;
-        document.getElementById('setNum').innerHTML = sessionStorage.getItem('pomoNum') + " POMO " + sessionStorage.getItem('setNum') + " SET";
-    }
-}
-
-function start_timer() {
-    if (timer !== null) {
-        alert('Please set the times first in the setting')
-        return;
-    }
-    let countDownDate = new Date().getTime() + parseFloat(value[0]) * 60000;
-
-    timer = setInterval(function () {
-        let now = new Date().getTime();
-        let distance = countDownDate - now;
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById('setPomoNum').innerHTML = pad(hours, 2) + ":" + pad(minutes, 2) + ":" + pad(seconds, 2);
-        if (distance < 0) {
-            pomoNum += 1;
-            sessionStorage.setItem("pomoNum", pomoNum);
-            sessionStorage.setItem("setNum", setNum);
-            if (pomoNum > 4) {
-                setNum += 1;
-                pomoNum = 0;
-                sessionStorage.setItem("setNum", setNum);
-                sessionStorage.setItem("pomoNum", pomoNum);
-                stop_timer();
-                //bigbreak_timer();
-            } else {
-                stop_timer();
-                //break_timer();
-            }
-            document.getElementById('numOfset').innerHTML = parseInt(sessionStorage.getItem('pomoNum')) + ` POMO Concentration`;
-        } else {
-            triggerResumeTimer(distance);
-        }
+function stateChanged(evt) {
+    setTimeout(() => {
+        evt.target.textContent = "Start";
+        currSetNum = sessionStorage.getItem('currSetNum');
+        numOfSet.textContent = `${currSetNum} POMO`;
+        settingPomoTime();
+        addStartHandler();
     }, 1000);
 }
 
-function triggerResumeTimer(countNum) {
-    document.getElementById('startbtn').innerHTML = `Pause`;
-    document.getElementById('startbtn').onclick = () => { stop_timer(countNum) };
-}
+function startTimer(evt) {
+    const nTimes4Count = parseFloat(sessionValue[0]);
+    minutes = minutes === 0 ? nTimes4Count * 60 : minutes;
+    timerHour = timerHour === 0 ? hours4Display : timerHour;
+    timerMinute = timerMinute === 0 ? minutes4Display : timerMinute;
+    nTimes4Compute = nTimes4Compute === 0 ? /\d\.\d/.test(sessionValue[0]) ? Math.floor(sessionValue[0]) : parseInt(sessionValue[0]) : nTimes4Compute;
 
-function resume_timer(countNum) {
-    let countDownDate = new Date().getTime() + countNum;
-    timer = setInterval(function () {
-        let now = new Date().getTime();
-        let distance = countDownDate - now;
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("setPomoNum").innerHTML = pad(hours, 2) + ":" + pad(minutes, 2) + ":" + pad(seconds, 2);
-        if (distance < 0) {
-            pomoNum += 1;
-            sessionStorage.setItem("pomoNum", pomoNum);
-            if (pomoNum > 4) {
-                setNum += 1;
-                pomoNum = 0;
-                sessionStorage.setItem("setNum", setNum);
-                sessionStorage.setItem("pomoNum", pomoNum);
-                stop_timer();
-                //bigbreak_timer();
-            } else {
-                stop_timer();
-                //break_timer();
-            }
-        } else {
-            triggerResumeTimer(distance);
+    evt.target.textContent = "Stop";
+    addStopHandler();
+
+    return new Promise(function(res, rej) {
+        for(let i = minutes; i >= 0; i--) {
+            tids.push(setTimeout(function() {
+                const computedNum = i >= 60 ? i - (nTimes4Compute) * 60 : i;
+                console.log(minutes, computedNum);
+                if(computedNum !== 60) {
+                    setPomoNum.textContent = `${pad(timerHour, 2)}:${pad(timerMinute, 2)}:${pad(computedNum, 2)}`;
+                }
+                if(i <= nTimes4Compute * 60) {
+                    if(timerHour > 0) {
+                        timerHour--;
+                        if(timerMinute > 0) {
+                            timerMinute--;
+                        } else {
+                            timerMinute = 59;
+                        }
+                    } else if(timerMinute > 0) {
+                        timerMinute--;
+                    }
+                    nTimes4Compute = timerHour * 60 + timerMinute;
+                }
+                minutes = i;
+                if(minutes === 0) {
+                    currSetNum++;
+                    sessionStorage.setItem('currSetNum', currSetNum);
+                    res();
+                }
+            }, (minutes - i) * 1000));
         }
-    }, 1000);
+    }).then(_ => stateChanged(evt));
 }
 
-function stop_timer(countNum) {
-    if(isValid(countNum)) {
-        clearInterval(timer);
-        document.getElementById('startbtn').innerHTML = `Resume`;
-        document.getElementById('startbtn').onclick = () => { resume_timer(countNum) };
+function addStartHandler() {
+    startBtn.removeEventListener('click', stopTimer);
+    startBtn.addEventListener('click', startTimer);
+}
+
+function addStopHandler() {
+    startBtn.removeEventListener('click', startTimer);
+    startBtn.addEventListener('click', stopTimer);
+}
+
+function stopTimer(evt) {
+    tids.forEach(clearTimeout);
+    evt.target.textContent = "Resume";
+    addStartHandler();
+}
+
+function settingPomoTime() {
+    setPomoNum.innerHTML = `${pad(hours4Display, 2)}:${pad(minutes4Display, 2)}:${pad(seconds4Display, 2)}`;
+}
+
+function init() {
+    if(!isNaN(parseInt(sessionValue[0]))) {
+        settingPomoTime();
+        startBtn.addEventListener('click', startTimer);
     } else {
-        clearInterval(timer);
-        isBeginning = 1;
-        timer = null;
-        document.getElementById('setPomoNum').innerHTML = pad(hours4Display, 2) + ":" + pad(minutes4Display, 2) + ":" + pad(seconds4Display, 2);
-        document.getElementById('startbtn').innerHTML = `Start`;
-        document.getElementById('setNum').innerHTML = sessionStorage.getItem('pomoNum') + " POMO " + sessionStorage.getItem('setNum') + " SET";
-        document.getElementById('startbtn').onclick = () => { start_timer() };
+        numOfSet.innerHTML = "Before you start, please set the times in the setting page first."
     }
+    sessionStorage.setItem('currSetNum', currSetNum);
 }
 
-function isValid(value) {
-    if(value !== undefined && value !== null && value !== '') {
-        return true;
-    }
-    return false;
-}
-
-document.querySelector('#startbtn').addEventListener('click', start_timer);
+init();
