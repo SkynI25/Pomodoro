d77dcd1508955837a39d348a6825f0ea11445617
SkynI25 <rlxo1651@daum.net>
2020-05-03 19:44:33 +0900

    mark unused codes

--- js/setting.js
+++ js/setting.js
@@ -3,12 +3,12 @@ const val2N = "Break Time";
 const val3N = "Big Break Time";
 const wrongInput = `wrong-input`;
 const pomoTime = document.querySelector('.pomoTime');
-const breakTime = document.querySelector('.breakTime');
-const bigbreakTime = document.querySelector('.bigbreakTime');
+// const breakTime = document.querySelector('.breakTime');
+// const bigbreakTime = document.querySelector('.bigbreakTime');
 const timesArr = [
     {type: pomoTime, range: 120},
-    {type: breakTime, range: 30},
-    {type: bigbreakTime, range: 60},
+    // {type: breakTime, range: 30},
+    // {type: bigbreakTime, range: 60},
 ];
 
 function pointWrongInput(data) {
@@ -25,36 +25,36 @@ function pointWrongInput(data) {
 function setChildValue() {
     let listVal = [];
     const val1 = timesArr[0].type.value;
-    const val2 = timesArr[1].type.value;
-    const val3 = timesArr[2].type.value;
+    // const val2 = timesArr[1].type.value;
+    // const val3 = timesArr[2].type.value;
 
     listVal.push(val1);
-    listVal.push(val2);
-    listVal.push(val3);
+    // listVal.push(val2);
+    // listVal.push(val3);
 
-    if(valiCheck(val1, timesArr[0].range) && valiCheck(val2, timesArr[1].range) && valiCheck(val3, timesArr[2].range)) {
+    if(valiCheck(val1, timesArr[0].range)) { // && valiCheck(val2, timesArr[1].range) && valiCheck(val3, timesArr[2].range)) {
         if(confirm("Save the times?")) {
             sessionStorage.setItem("timeSetting", listVal);
             window.location.href='main.html';
         }
     } else {
         pointWrongInput(timesArr[0]);
-        pointWrongInput(timesArr[1]);
-        pointWrongInput(timesArr[2]);
+        // pointWrongInput(timesArr[1]);
+        // pointWrongInput(timesArr[2]);
         alert(`Please setting the time with a valid number.`);
     }
 }
 
 function valiCheck(data,range) {
-    return !/\D/.test(data) && Number(data) > 0 && Number(data) <= range; 
+    return (!/\D/.test(data) || /\d\.\d/.test(data)) && Number(data) > 0 && Number(data) <= range; 
 }
 
 function initInput() {
     if(sessionStorage.getItem('timeSetting')) {
         const values = sessionStorage.getItem('timeSetting').split(",");
         pomoTime.value = values[0];
-        breakTime.value = values[1];
-        bigbreakTime.value = values[2];
+        // breakTime.value = values[1];
+        // bigbreakTime.value = values[2];
     }
 }
 
