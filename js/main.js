e051dea3e8c18a36379010e7a9c1ad3ce4128928
SkynI25 <rlxo1651@daum.net>
2020-05-03 19:44:01 +0900

    split background updating codes to bg.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
+const todayText = document.querySelector('#date');
+const startBtn = document.querySelector('.buttonLocation > button');
+
 const today = new Date();
-const day = today.getDate() < 10 ? `0${today.getDate()+1}` : today.getDate()+1;
+const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
 const month = today.getMonth() < 10 ? `0${today.getMonth()+1}` : today.getMonth()+1;
 const year = today.getFullYear();
 const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const dayOfWeek = week[today.getDay()];
-document.querySelector('#date').innerHTML = `${year}. ${month}. ${day}. (${dayOfWeek})`;
+todayText.innerHTML = `${year}. ${month}. ${day}. (${dayOfWeek})`;
 
 function clickHandler() {
     if(sessionStorage.getItem('timeSetting') !== null) {
         window.location.href='pomodoro.html';
+    } else {
+        alert("Please set the times in the Setting before you start Pomodoro.")
     }
-    alert("Please set the times in the Setting before you start Pomodoro.")
 }
 
-document.querySelector('.buttonLocation > button').addEventListener('click', clickHandler);
+function init() {
+    startBtn.addEventListener('click', clickHandler);
+}
+
+init();
