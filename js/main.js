56b37709dc27880d1c01893e59a7de82b07b4e40
SkynI25 <rlxo1651@daum.net>
2020-05-11 17:23:17 +0900

    add instruction feature

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import menuIcon from '../img/menu.svg';
 
 const todayText = document.querySelector('#date');
-const startBtn = document.querySelector('.buttonLocation > button');
+const startBtn = document.querySelector('.buttonLocation button');
+const clearBtn = document.querySelector('.buttonLocation h1');
 const menu_img = document.querySelector('.menu-img');
+const questionMark = document.querySelector('.buttonLocation h2');
 
 const today = new Date();
 const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
@@ -12,11 +14,42 @@ const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const dayOfWeek = week[today.getDay()];
 todayText.innerHTML = `${year}. ${month}. ${day}. (${dayOfWeek})`;
 
-function clickHandler() {
+function startBtnHandler() {
     if(localStorage.getItem('timeSetting') !== null) {
         window.location.href='pomodoro.html';
     } else {
-        alert("Please set the times in the Setting before you start Pomodoro.")
+        alert("시작하시기 전에 세팅 화면에서 시간을 설정해주세요.")
+    }
+}
+
+function clearBtnHandler() {
+    if(confirm('설정한 모든 값을 지우시겠습니까?')) {
+        localStorage.clear();
+        window.location.reload();
+    }
+}
+
+function coverHandler() {
+    const body = document.querySelector('body');
+    const cover = body.querySelector('div');
+
+    if(cover.classList.contains('instruction-cover')) {
+        cover.classList.remove('instruction-cover');
+    };
+}
+
+function instructionHandler() {
+    const body = document.querySelector('body');
+    const cover = body.querySelector('div');
+
+    body.removeEventListener('click', coverHandler);
+    if(!cover.classList.contains('instruction-cover')) {
+        cover.classList.add('instruction-cover');
+        setTimeout(() => {
+            body.addEventListener('click', coverHandler);
+        });
+    } else {
+        cover.classList.remove('instruction-cover');
     }
 }
 
@@ -31,7 +64,9 @@ function imgSetting() {
 function init() {
     imgSetting();
     menu_img.addEventListener('click', imgHandler);
-    startBtn.addEventListener('click', clickHandler);
+    startBtn.addEventListener('click', startBtnHandler);
+    clearBtn.addEventListener('click', clearBtnHandler);
+    questionMark.addEventListener('click', instructionHandler);
 }
 
 init();
