916b7a16d536f6ae290968365c6114845747629c
SkynI25 <rlxo1651@daum.net>
2020-05-28 18:47:22 +0900

    refactoring codes & remove images

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
+
 import menuIcon from '../img/menu.svg';
-
-const todayText = document.querySelector('#date');
+const USER_NAME = 'username';
+const infoSect = document.querySelector('.infoSect');
+const topSect = document.querySelector('.topSect');
 const startBtn = document.querySelector('.buttonLocation button');
-const clearBtn = document.querySelector('.buttonLocation h1');
 const menu_img = document.querySelector('.menu-img');
-const questionMark = document.querySelector('.buttonLocation h2');
-
-const today = new Date();
-const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
-const month = today.getMonth() < 10 ? `0${today.getMonth()+1}` : today.getMonth()+1;
-const year = today.getFullYear();
-const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const dayOfWeek = week[today.getDay()];
-todayText.innerHTML = `${year}. ${month}. ${day}. (${dayOfWeek})`;
 
 function startBtnHandler() {
     if(localStorage.getItem('timeSetting') !== null) {
@@ -22,51 +14,51 @@ function startBtnHandler() {
     }
 }
 
-function clearBtnHandler() {
-    if(confirm('설정한 모든 값을 지우시겠습니까?')) {
-        localStorage.clear();
-        window.location.reload();
-    }
-}
-
-function coverHandler() {
-    const body = document.querySelector('body');
-    const cover = body.querySelector('div');
-
-    if(cover.classList.contains('instruction-cover')) {
-        cover.classList.remove('instruction-cover');
-    };
-}
-
-function instructionHandler() {
-    const body = document.querySelector('body');
-    const cover = body.querySelector('div');
-
-    body.removeEventListener('click', coverHandler);
-    if(!cover.classList.contains('instruction-cover')) {
-        cover.classList.add('instruction-cover');
-        setTimeout(() => {
-            body.addEventListener('click', coverHandler);
-        });
-    } else {
-        cover.classList.remove('instruction-cover');
-    }
-}
-
 function imgHandler() {
-    window.location.href = './setting.html';
+    window.location.href = './index.html';
 }
 
 function imgSetting() {
     menu_img.src = `./public/${menuIcon}`;
 }
 
+function greetingMSG() {
+    let greeting = "";
+    const now = new Date().getHours();
+    if(now < 12) {
+        greeting = "Good morning";
+    } else if(now >= 12 && now < 17) {
+        greeting = "Good afternoon";
+    } else {
+        greeting = "Good evening";
+    }
+    return greeting;
+}
+
+function hello2User() {
+    const h1 = infoSect.querySelector('h1');
+    h1.textContent = `${greetingMSG()}, ${localStorage.getItem(USER_NAME)}`;
+}
+
+function getUserName() {
+    if(localStorage.getItem(USER_NAME)) {
+        hello2User();
+        welcomeBar();
+    }
+}
+
+function welcomeBar() {
+    const cover = document.createElement('div');
+    cover.textContent = `Welcome ${localStorage.getItem(USER_NAME)}`;
+    cover.classList.add("topSect-cover");
+    topSect.prepend(cover);
+}
+
 function init() {
     imgSetting();
+    getUserName();
     menu_img.addEventListener('click', imgHandler);
     startBtn.addEventListener('click', startBtnHandler);
-    clearBtn.addEventListener('click', clearBtnHandler);
-    questionMark.addEventListener('click', instructionHandler);
 }
 
 init();
